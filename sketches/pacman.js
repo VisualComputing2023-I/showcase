6c32c4b97dcaa614c42fbd3ae9e1fafc7847700a
jupachonc <jupachonc@users.noreply.github.com>
2023-03-13 03:22:06 +0000

    deploy: bbd3c0d731780166a3d17529a8cebd1601afe0f2

---
+++ sketches/pacman.js
@@ -0,0 +1,27 @@
+let x = 900
+let flag = true
+
+function setup() {
+  createCanvas(900, 300);
+  pacman = loadImage('./assets/pacman.png');
+}
+
+function draw() {
+  // Image background
+  image(pacman, 0, 0);
+  
+  // Move lines
+  flag ? x-- : x++
+  
+  // Change direction
+  if(x < 0) flag = false
+  if(x > width) flag = true
+  
+  
+  // Vertical lines
+  for (let i = x; i < width ; i+=18) {
+    fill(color('black'));
+    rect(i, 0, 13, 247);
+  } 
+ 
+}
